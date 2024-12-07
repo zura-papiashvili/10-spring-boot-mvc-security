9d326c9c4d9d4c6f5872a1841d3f1b971b7b8f6a
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-07 15:16:44 -0500

    feat: login

---
+++ src/main/java/com/springcourse/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.springcourse.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class DemoSecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("MANAGER", "EMPLOYEE")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("ADMIN", "EMPLOYEE", "MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+
+    }
+
+    // change the default login page
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll());
+
+        return http.build();
+    }
+}
